13b1a6fd4700558d7f478e8445e86411b92d200d
bors-libra <bors-libra@users.noreply.github.com>
2021-02-04 02:23:09 +0000

    deploy: c8b69e426bac283479b2c904159521f638d0befa

---
+++ bytecode/spec_instrumentation/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["SpecInstrumenterProcessor","Spec Instrumenter"]]});
---
+++ diem_client/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Method",""],["MethodRequest",""],["MethodResponse",""]],"mod":[["errors",""],["views",""]],"struct":[["AccountAddress","A struct that represents an account address."],["BlockingClient",""],["Client",""],["Error",""],["JsonRpcRequest",""],["Response",""],["SignedTransaction","A transaction that has been signed."],["State",""]],"type":[["Result",""]]});
